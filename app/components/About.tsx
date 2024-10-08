bac3f8953b6ed8aed59b2af21067e776fd182f40
BushraAli834 <muhammadali132078@gmail.com>
2024-10-08 13:54:31 +0500

    Add files via upload

---
+++ app/components/About.tsx
@@ -0,0 +1,8 @@
+function About(){
+    return(
+        <div>
+        <h1><i>Bushra Ali</i></h1>
+        </div>
+    )
+}
+export default About;
---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <div>
+        <h1>Wellcome To My Website</h1>
+        </div>
+    )
+}
+export default Header;
